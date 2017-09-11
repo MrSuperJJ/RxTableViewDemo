246df257d5c11bf82c264aa1107eacea34615f17
yejiajun <yejiajun@cmhi.chinamobile.com>
2017-09-11 10:14:35 +0800

    init

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘8.0’
+use_frameworks!
+
+target 'RxTableViewDemo' do
+pod 'RxSwift'    
+pod 'RxCocoa'
+pod ‘RxDataSources’
+end
---
+++ RxTableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxTableViewDemo
+//
+//  Created by 叶佳骏 on 2017/9/11.
+//  Copyright © 2017年 Mr.JJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
